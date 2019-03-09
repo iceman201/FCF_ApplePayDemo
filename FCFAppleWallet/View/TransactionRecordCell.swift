ee39a04cab09184a99e7d3435d2bc1ef8ee84c24
Liguo Jiao <liguo@jiao.co.nz>
2019-03-10 00:32:32 +1300

    Time for sleep

--- FCFAppleWallet/View/TransactionRecordCell.swift
+++ FCFAppleWallet/View/TransactionRecordCell.swift
@@ -9,16 +9,54 @@
 import UIKit
 
 class TransactionRecordCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    var topLineLabel: UILabel!
+    var subLineLabel: UILabel!
+    var dateLineLabel: UILabel!
+    var amountLabel: UILabel!
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        loadView()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    fileprivate func loadView() {
+        self.selectionStyle = .none
+        
+        let divider = UIView()
+        divider.backgroundColor = UIColor.lightGray.withAlphaComponent(0.3)
+        self.addSubview(divider)
+        divider.translatesAutoresizingMaskIntoConstraints = false
+        divider.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        divider.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        divider.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding * 2).isActive = true
+        divider.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding * 2).isActive = true
+        
+        
+        let container = UIView()
+        self.contentView.addSubview(container)
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.heightAnchor.constraint(equalTo: self.contentView.heightAnchor).isActive = true
+        container.widthAnchor.constraint(equalTo: self.contentView.widthAnchor).isActive = true
+        
+        let top = UILabel()
+        top.text = "hahahah"
+        top.textColor = #colorLiteral(red: 0.2156862745, green: 0.2156862745, blue: 0.2156862745, alpha: 1)
+        container.addSubview(top)
+        top.translatesAutoresizingMaskIntoConstraints = false
+        top.topAnchor.constraint(equalTo: divider.topAnchor, constant: padding).isActive = true
+        top.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: padding * 2).isActive = true
+        
+        let sub = UILabel()
+        sub.textColor = #colorLiteral(red: 0.2156862745, green: 0.2156862745, blue: 0.2156862745, alpha: 1).withAlphaComponent(0.5)
+        sub.text = "lalala"
+        container.addSubview(sub)
+        sub.translatesAutoresizingMaskIntoConstraints = false
+        sub.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: padding * 2).isActive = true
+        sub.topAnchor.constraint(equalTo: top.bottomAnchor, constant: padding).isActive = true
     }
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
