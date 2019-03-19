76e08204fc3344a190a0af53bc0f03fc02d66b3c
Liguo Jiao <liguo@jiao.co.nz>
2019-03-20 01:02:28 +1300

    Create StatementCell.swift

---
+++ FCFAppleWallet/View/TableCell/StatementCell.swift
@@ -0,0 +1,18 @@
+//
+//  StatementCell.swift
+//  FCFAppleWallet
+//
+//  Created by Liguo Jiao on 19/03/19.
+//  Copyright © 2019 Liguo Jiao. All rights reserved.
+//
+
+import UIKit
+
+class StatementCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
