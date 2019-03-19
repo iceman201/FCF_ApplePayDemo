ebcd936227feaa10101329e60c360e3044d75bf5
Liguo Jiao <liguo@jiao.co.nz>
2019-03-20 01:23:57 +1300

    Update StatementCell.swift

--- FCFAppleWallet/View/TableCell/StatementCell.swift
+++ FCFAppleWallet/View/TableCell/StatementCell.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class StatementCell: UITableViewCell {
 
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 }
